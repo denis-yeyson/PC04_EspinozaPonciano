5d6b759f9d59960b92ea94a09b6355d55ce6ff93
DENIS YEYSON ESPINOZA PONCIANO <denisyeyson@outlook.com>
2021-11-27 01:20:38 -0500

    Actualizacion

--- app/src/main/java/com/denisyeyson/pc04_espinozaponciano/MainActivity2.java
+++ app/src/main/java/com/denisyeyson/pc04_espinozaponciano/MainActivity2.java
@@ -1,14 +1,63 @@
 package com.denisyeyson.pc04_espinozaponciano;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.denisyeyson.pc04_espinozaponciano.adaptador.ListaProductosAdapter;
+import com.denisyeyson.pc04_espinozaponciano.entidades.Productos;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    DatabaseReference base_de_datos;
+    List<Productos> listaProductos= new ArrayList<>();
+    RecyclerView rvProductos;
+    ListaProductosAdapter adaptador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        base_de_datos = FirebaseDatabase.getInstance().getReference();
+        rvProductos = findViewById(R.id.rvProductos);
+        rvProductos.setLayoutManager(new GridLayoutManager(this,1));
+        obtenerProductos();
+
+    }
+
+    public void obtenerProductos(){
+        listaProductos.clear();
+
+        base_de_datos.child("Productos").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot objeto: snapshot.getChildren()){
+                    listaProductos.add(objeto.getValue(Productos.class));
+                }
+                adaptador = new ListaProductosAdapter(MainActivity2.this,listaProductos);
+                rvProductos.setAdapter(adaptador);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/denisyeyson/pc04_espinozaponciano/RegistroProductoActivity.java
+++ app/src/main/java/com/denisyeyson/pc04_espinozaponciano/RegistroProductoActivity.java
@@ -17,19 +17,17 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class RegistroProductoActivity extends AppCompatActivity {
 
-    EditText txtNombre,txtMarca,txtDescripcion,txtCantidad,txtPrecio;
-    Button btnAgregarProducto;
+    EditText txtNombre, txtMarca, txtDescripcion, txtCantidad, txtPrecio;
+    Button btnAgregarProducto,btnListarProductoNuevo;
 
-    String nombre,marca,descripcion,cantidad,precio;
-
-    FirebaseAuth autentificacion;
-    DatabaseReference base_de_datos;
+    String nombre, marca, descripcion, cantidad, precio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,48 +40,59 @@ public class RegistroProductoActivity extends AppCompatActivity {
         txtCantidad = findViewById(R.id.txtCantidad);
         txtPrecio = findViewById(R.id.txtPrecio);
         btnAgregarProducto = findViewById(R.id.btnAgregarProducto);
+        btnListarProductoNuevo = findViewById(R.id.btnListarProductoNuevo);
 
         btnAgregarProducto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 nombre = txtNombre.getText().toString().trim();
                 marca = txtMarca.getText().toString().trim();
                 descripcion = txtDescripcion.getText().toString().trim();
                 cantidad = txtCantidad.getText().toString().trim();
                 precio = txtPrecio.getText().toString().trim();
-
                 if (nombre.isEmpty() || marca.isEmpty() || descripcion.isEmpty() || cantidad.isEmpty() || precio.isEmpty()) {
-                    Toast.makeText(RegistroProductoActivity.this, "Debe completar todos los campos para registrar", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(RegistroProductoActivity.this, "Debe completar todos los campos para registrar", Toast.LENGTH_SHORT).show();
+                        } else {
+                            registrarProducto(nombre, marca, descripcion, cantidad, precio);
+                        }
+                    }
+        });
+
+        btnListarProductoNuevo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegistroProductoActivity.this, MainActivity2.class));
+            }
+        });
+    }
+
+    public void registrarProducto(String nombre, String marca, String descripcion, String cantidad, String precio) {
+        DatabaseReference base_de_datos = FirebaseDatabase.getInstance().getReference();;
+        Map<String, Object> map = new HashMap<>();
+        map.put("nombre", nombre);
+        map.put("marca", marca);
+        map.put("descripcion", descripcion);
+        map.put("cantidad", cantidad);
+        map.put("precio", precio);
+
+        String key = base_de_datos.child("Productos").push().getKey();
+
+        base_de_datos.child("Productos").child(key).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task4) {
+                if (task4.isSuccessful()) {
+                    Toast.makeText(RegistroProductoActivity.this, "Producto Agregado correctamente", Toast.LENGTH_SHORT).show();
+                    finish();
+                    startActivity(new Intent(RegistroProductoActivity.this, MainActivity2.class));
                 } else {
-                    registrarProducto(nombre, marca, descripcion, cantidad, precio);
+                    Toast.makeText(RegistroProductoActivity.this, "No se pudo guardar los datos en la Base de Datos", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
 
-    public void registrarProducto(String nombre, String marca,String descripcion,String cantidad,String precio){
-       Map<String,Object> map = new HashMap<>();
-            map.put("nombre",nombre);
-            map.put("marca",marca);
-            map.put("descripcion",descripcion);
-            map.put("cantidad",cantidad);
-            map.put("pecio",precio);
-            base_de_datos.child("Productos").child(nombre).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
-                @Override
-                public void onComplete(@NonNull Task<Void> task2) {
-                    if(task2.isSuccessful()){
-                        Toast.makeText(RegistroProductoActivity.this, "ProductoAgregado correctamente", Toast.LENGTH_SHORT).show();
-
-                        startActivity(new Intent(RegistroProductoActivity.this,MainActivity2.class));
-                        finish();
-                    }else{
-                        Toast.makeText(RegistroProductoActivity.this, "No se pudo guardar los datos en la Base de Datos", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-
-    }
-    public void limpiar(){
+    public void limpiar() {
         txtNombre.setText("");
         txtMarca.setText("");
         txtDescripcion.setText("");
